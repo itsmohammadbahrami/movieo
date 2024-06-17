ae33c3a2702eab33839a770e12af2ce08bea6289
itsmohammadbahrami <itsmohammadbahrami@gmail.com>
2024-06-17 17:51:17 +0330

    style: update font sizes

--- components/Movies/Movies/movie/movie-details/MovieDetails.tsx
+++ components/Movies/Movies/movie/movie-details/MovieDetails.tsx
@@ -7,10 +7,10 @@ type MovieDetailsProps = {
 
 const MovieDetails: React.FC<MovieDetailsProps> = ({ movie }) => (
   <div className="flex flex-row justify-between items-center mt-1">
-    <div className="text-[11px] w-1/2 text-gray-200 truncate">
+    <div className="text-xs w-1/2 text-gray-200 truncate">
       {movie.movie_title}
     </div>
-    <div className="text-[11px] font-normal bg-[#303030] rounded-2xl px-1">
+    <div className="text-xs font-normal bg-[#303030] rounded-2xl px-1">
       {string.movies.rate} {movie.rate_avrage}
     </div>
   </div>
--- components/Movies/Movies/movie/movie-image/hover-overlay/HoverOverlay.tsx
+++ components/Movies/Movies/movie/movie-image/hover-overlay/HoverOverlay.tsx
@@ -9,7 +9,7 @@ type Props = {
 const HoverOverlay: React.FC<Props> = ({ movie }) => (
   <div className="absolute space-y-3 rounded-lg top-0 left-0 w-full h-full flex justify-end flex-col items-start p-2 bg-gradient-to-t from-[#272727] via-transparent to-transparent">
 
-    <div className="p-1 bg-white rounded-2xl items-center justify-center bg-opacity-10 text-[10px] flex flex-row">
+    <div className="p-1 bg-white rounded-2xl items-center justify-center bg-opacity-10 text-xs flex flex-row">
       <MdThumbUpAlt size={14} className="ml-1" />
       {movie.avg_rate_label}
     </div>
