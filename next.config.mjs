ecc74bf2b264bd3dc57e6e286e166a82e84e9377
itsmohammadbahrami <itsmohammadbahrami@gmail.com>
2024-06-17 15:35:31 +0330

    feat: update next configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "static.cdn.asset.filimo.com",
+            },
+        ],
+    },
+};
 
 export default nextConfig;
