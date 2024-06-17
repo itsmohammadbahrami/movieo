dadb4ede8960ddb650d059905b7d7600a960a436
itsmohammadbahrami <itsmohammadbahrami@gmail.com>
2024-06-17 17:19:21 +0330

    feat: add home components

--- components/Home/useHomePage.tsx
+++ components/Home/useHomePage.tsx
@@ -1,5 +1,4 @@
-import { RootState, selectFilteredAndSortedMovies } from "@/store";
-import { useAppSelector } from "@/store";
+import { RootState, useAppSelector, selectFilteredAndSortedMovies } from "@/store";
 
 const useHomePage = () => {
   const { filter, sort } = useAppSelector((state: RootState) =>
@@ -7,7 +6,9 @@ const useHomePage = () => {
   );
 
   const query: { category?: string; sort?: string } = {};
+
   if (sort) query.sort = sort;
+
   if (filter) query.category = filter;
 
   return query;
--- components/index.ts
+++ components/index.ts
@@ -1,2 +1,2 @@
 export * from "./Movies";
-export * from "./Home";
+export * from "./home";
